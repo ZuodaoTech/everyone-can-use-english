8dbe089c810542a32970925d79bd309df38074e4
an-lee <an.lee.work@gmail.com>
2024-01-13 22:51:06 +0800

    fix post audio player

--- enjoy/src/renderer/components/posts/post-audio.tsx
+++ enjoy/src/renderer/components/posts/post-audio.tsx
@@ -6,12 +6,83 @@ import { Button, Skeleton } from "@renderer/components/ui";
 import { PlayIcon, PauseIcon } from "lucide-react";
 import { useIntersectionObserver } from "@uidotdev/usehooks";
 import { secondsToTimestamp } from "@renderer/lib/utils";
+import { MediaPlayer, MediaProvider } from "@vidstack/react";
+import {
+  DefaultAudioLayout,
+  defaultLayoutIcons,
+} from "@vidstack/react/player/layouts/default";
+export const STORAGE_WORKER_ENDPOINT = "https://enjoy-storage.baizhiheizi.com";
 
 export const PostAudio = (props: {
   audio: Partial<MediumType>;
   height?: number;
 }) => {
   const { audio, height = 80 } = props;
+  const [currentTime, setCurrentTime] = useState<number>(0);
+  const { webApi } = useContext(AppSettingsProviderContext);
+  const [transcription, setTranscription] = useState<TranscriptionType>();
+
+  const currentTranscription = (transcription?.result || []).find(
+    (s) =>
+      currentTime >= s.offsets.from / 1000.0 &&
+      currentTime <= s.offsets.to / 1000.0
+  );
+
+  useEffect(() => {
+    webApi
+      .transcriptions({
+        targetMd5: audio.md5,
+      })
+      .then((response) => {
+        setTranscription(response?.transcriptions?.[0]);
+      });
+  }, [audio.md5]);
+
+  return (
+    <div className="w-full">
+      {audio.sourceUrl.startsWith(STORAGE_WORKER_ENDPOINT) ? (
+        <WavesurferPlayer
+          currentTime={currentTime}
+          setCurrentTime={setCurrentTime}
+          audio={audio}
+          height={height}
+        />
+      ) : (
+        <MediaPlayer
+          onTimeUpdate={({ currentTime: _currentTime }) => {
+            setCurrentTime(_currentTime);
+          }}
+          src={audio.sourceUrl}
+        >
+          <MediaProvider />
+          <DefaultAudioLayout icons={defaultLayoutIcons} />
+        </MediaPlayer>
+      )}
+
+      {currentTranscription && (
+        <div className="mt-2 bg-muted px-4 py-2 rounded">
+          <div className="text-muted-foreground text-center font-serif">
+            {currentTranscription.text}
+          </div>
+        </div>
+      )}
+
+      {audio.coverUrl && (
+        <div className="mt-2">
+          <img src={audio.coverUrl} className="w-full rounded" />
+        </div>
+      )}
+    </div>
+  );
+};
+
+const WavesurferPlayer = (props: {
+  audio: Partial<MediumType>;
+  height?: number;
+  currentTime: number;
+  setCurrentTime: (currentTime: number) => void;
+}) => {
+  const { audio, height = 80, currentTime, setCurrentTime } = props;
   const [initialized, setInitialized] = useState(false);
   const [isPlaying, setIsPlaying] = useState(false);
   const [wavesurfer, setWavesurfer] = useState(null);
@@ -20,15 +91,6 @@ export const PostAudio = (props: {
     threshold: 1,
   });
   const [duration, setDuration] = useState<number>(0);
-  const { webApi } = useContext(AppSettingsProviderContext);
-  const [currentTime, setCurrentTime] = useState<number>(0);
-  const [transcription, setTranscription] = useState<TranscriptionType>();
-
-  const currentTranscription = (transcription?.result || []).find(
-    (s) =>
-      currentTime >= s.offsets.from / 1000.0 &&
-      currentTime <= s.offsets.to / 1000.0
-  );
 
   const onPlayClick = useCallback(() => {
     wavesurfer.isPlaying() ? wavesurfer.pause() : wavesurfer.play();
@@ -93,18 +155,8 @@ export const PostAudio = (props: {
     };
   }, [wavesurfer]);
 
-  useEffect(() => {
-    webApi
-      .transcriptions({
-        targetMd5: audio.md5,
-      })
-      .then((response) => {
-        setTranscription(response?.transcriptions?.[0]);
-      });
-  }, [audio.md5]);
-
   return (
-    <div className="w-full">
+    <>
       <div className="flex justify-end">
         <span className="text-xs text-muted-foreground">
           {secondsToTimestamp(duration)}
@@ -141,20 +193,6 @@ export const PostAudio = (props: {
           ref={containerRef}
         ></div>
       </div>
-
-      {currentTranscription && (
-        <div className="mt-2 bg-muted px-4 py-2 rounded">
-          <div className="text-muted-foreground text-center font-serif">
-            {currentTranscription.text}
-          </div>
-        </div>
-      )}
-
-      {audio.coverUrl && (
-        <div className="mt-2">
-          <img src={audio.coverUrl} className="w-full rounded" />
-        </div>
-      )}
-    </div>
+    </>
   );
 };
