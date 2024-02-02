71b19fa9ce3547f83b1d6470b7034f8e5175afd1
Percy Lee <PercyAtFj@users.noreply.github.com>
2024-02-02 13:57:41 +0800

    Fix bug (#190) (#241)
    Co-authored-by: lixiancan <lixc.percy@gmail.com>

--- enjoy/src/main.ts
+++ enjoy/src/main.ts
@@ -58,9 +58,7 @@ app.on("ready", async () => {
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
     app.quit();
-  }
 });
 
 app.on("activate", () => {
