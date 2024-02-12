b8011d20d73320b588c041a56e62829925a6d28f
an-lee <an.lee.work@gmail.com>
2024-02-12 10:42:25 +0800

    Test: add e2e tests (#297)
    * add e2e test
    
    * add whisper & ffmpeg command validate test
    
    * remove tests-examples

---
+++ enjoy/e2e/main.spec.ts
@@ -0,0 +1,73 @@
+import { expect, test } from "@playwright/test";
+import {
+  clickMenuItemById,
+  findLatestBuild,
+  ipcMainCallFirstListener,
+  ipcRendererCallFirstListener,
+  parseElectronApp,
+  ipcMainInvokeHandler,
+  ipcRendererInvoke,
+} from "electron-playwright-helpers";
+import { ElectronApplication, Page, _electron as electron } from "playwright";
+
+declare global {
+  interface Window {
+    __ENJOY_APP__: any;
+  }
+}
+
+let electronApp: ElectronApplication;
+
+test.beforeAll(async () => {
+  // find the latest build in the out directory
+  const latestBuild = findLatestBuild();
+  // parse the directory and find paths and other info
+  const appInfo = parseElectronApp(latestBuild);
+  // set the CI environment variable to true
+  process.env.CI = "e2e";
+  electronApp = await electron.launch({
+    args: [appInfo.main],
+    executablePath: appInfo.executable,
+  });
+  electronApp.on("window", async (page) => {
+    const filename = page.url()?.split("/").pop();
+    console.log(`Window opened: ${filename}`);
+
+    // capture errors
+    page.on("pageerror", (error) => {
+      console.error(error);
+    });
+    // capture console messages
+    page.on("console", (msg) => {
+      console.log(msg.text());
+    });
+  });
+});
+
+test.afterAll(async () => {
+  await electronApp.close();
+});
+
+let page: Page;
+
+test("renders the first page", async () => {
+  page = await electronApp.firstWindow();
+  const title = await page.title();
+  expect(title).toBe("Enjoy");
+});
+
+test("validate whisper command", async () => {
+  page = await electronApp.firstWindow();
+  const res = await page.evaluate(() => {
+    return window.__ENJOY_APP__.whisper.check();
+  });
+  expect(res.success).toBeTruthy();
+});
+
+test("valid ffmpeg command", async () => {
+  page = await electronApp.firstWindow();
+  const res = await page.evaluate(() => {
+    return window.__ENJOY_APP__.ffmpeg.check();
+  });
+  expect(res).toBeTruthy();
+});
