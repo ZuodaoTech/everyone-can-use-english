8ccec967aceb029d44a1ec17069a08a3347ff1b2
an-lee <an.lee.work@gmail.com>
2024-02-18 16:02:05 +0800

    refactor whisper

--- enjoy/src/main/whisper.ts
+++ enjoy/src/main/whisper.ts
@@ -26,26 +26,10 @@ class Whipser {
     } else {
       this.binMain = path.join(__dirname, "lib", "whisper", "main");
     }
+    this.initialize();
   }
 
-  currentModel() {
-    if (!this.config.availableModels) return;
-
-    let model: WhisperConfigType["availableModels"][0];
-    if (this.config.model) {
-      model = (this.config.availableModels || []).find(
-        (m) => m.name === this.config.model
-      );
-    }
-    if (!model) {
-      model = this.config.availableModels[0];
-    }
-
-    settings.setSync("whisper.model", model.name);
-    return model.savePath;
-  }
-
-  async initialize() {
+  initialize() {
     const models = [];
 
     const bundledModels = fs.readdirSync(this.bundledModelsDir);
@@ -74,44 +58,26 @@ class Whipser {
     settings.setSync("whisper.availableModels", models);
     settings.setSync("whisper.modelsPath", dir);
     this.config = settings.whisperConfig();
+  }
 
-    const command = `"${this.binMain}" --help`;
-    logger.debug(`Checking whisper command: ${command}`);
-    return new Promise((resolve, reject) => {
-      exec(
-        command,
-        {
-          timeout: PROCESS_TIMEOUT,
-        },
-        (error, stdout, stderr) => {
-          if (error) {
-            logger.error("error", error);
-          }
+  currentModel() {
+    if (!this.config.availableModels) return;
 
-          if (stderr) {
-            logger.debug("stderr", stderr);
-          }
-
-          if (stdout) {
-            logger.debug("stdout", stdout);
-          }
-
-          const std = (stdout || stderr).toString()?.trim();
-          if (std.startsWith("usage:")) {
-            resolve(true);
-          } else {
-            reject(
-              error || new Error("Whisper check failed: unknown error").message
-            );
-          }
-        }
+    let model: WhisperConfigType["availableModels"][0];
+    if (this.config.model) {
+      model = (this.config.availableModels || []).find(
+        (m) => m.name === this.config.model
       );
-    });
+    }
+    if (!model) {
+      model = this.config.availableModels[0];
+    }
+
+    settings.setSync("whisper.model", model.name);
+    return model.savePath;
   }
 
   async check() {
-    await this.initialize();
-
     const model = this.currentModel();
     logger.debug(`Checking whisper model: ${model}`);
 
@@ -263,7 +229,7 @@ class Whipser {
   registerIpcHandlers() {
     ipcMain.handle("whisper-config", async () => {
       try {
-        await this.initialize();
+        await this.check();
         return Object.assign({}, this.config, { ready: true });
       } catch (_err) {
         return Object.assign({}, this.config, { ready: false });
@@ -295,7 +261,7 @@ class Whipser {
     ipcMain.handle("whisper-set-service", async (event, service) => {
       if (service === "local") {
         try {
-          await this.initialize();
+          await this.check();
           settings.setSync("whisper.service", service);
           this.config.service = service;
           return this.config;
