ad1788d4f4575b3336c46170a4da3af9f18c2745
an-lee <an.lee.work@gmail.com>
2024-02-19 17:34:28 +0800

    add test for login

--- enjoy/e2e/main.spec.ts
+++ enjoy/e2e/main.spec.ts
@@ -18,6 +18,15 @@ declare global {
   }
 }
 
+const user = {
+  id: 24000001,
+  name: "李安",
+  avatarUrl:
+    "https://mixin-images.zeromesh.net/9tMscDkZuXyLKMRChmFi5IiFF2XuQHO8PQpED8zKOCBDGKGSVB9J2eqzyjhgJKPDVunXiT-DPiisImX_bhBDPi4=s256",
+  accessToken:
+    "eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOm51bGwsInNpZCI6IjkyN2RjNGRhLTI3YTItNDU5MC1hY2ZiLWMxYTJmZjhhMmFjMiIsInVpZCI6MjQwMDAwMDEsImlhdCI6MTcwODMyODk1N30.PCN_SZ7JH-VYLl56XU8kxYN9Cy44sO13mBQNNz6x-pa",
+};
+
 let electronApp: ElectronApplication;
 const resultDir = path.join(process.cwd(), "test-results");
 
@@ -56,33 +65,61 @@ test.afterAll(async () => {
   await electronApp.close();
 });
 
-test("renders the first page", async () => {
-  const page = await electronApp.firstWindow();
-  const title = await page.title();
-  expect(title).toBe("Enjoy");
-});
+test.describe("main dependencies", () => {
+  test("validate whisper command", async () => {
+    const page = await electronApp.firstWindow();
+    const res = await page.evaluate(() => {
+      return window.__ENJOY_APP__.whisper.check();
+    });
+    console.info(res.log);
+    expect(res.success).toBeTruthy();
 
-test("validate whisper command", async () => {
-  const page = await electronApp.firstWindow();
-  const res = await page.evaluate(() => {
-    return window.__ENJOY_APP__.whisper.check();
+    const settings = fs.readJsonSync(path.join(resultDir, "settings.json"));
+    expect(settings.whisper.service).toBe("local");
   });
-  console.info(res.log);
-  expect(res.success).toBeTruthy();
 
-  const settings = fs.readJsonSync(path.join(resultDir, "settings.json"));
-  expect(settings.whisper.service).toBe("local");
-});
-
-test("valid ffmpeg command", async () => {
-  const page = await electronApp.firstWindow();
-  const res = await page.evaluate(() => {
-    return window.__ENJOY_APP__.ffmpeg.check();
+  test("valid ffmpeg command", async () => {
+    const page = await electronApp.firstWindow();
+    const res = await page.evaluate(() => {
+      return window.__ENJOY_APP__.ffmpeg.check();
+    });
+    expect(res).toBeTruthy();
   });
-  expect(res).toBeTruthy();
+
+  test("should setup default library path", async () => {
+    const settings = fs.readJsonSync(path.join(resultDir, "settings.json"));
+    expect(settings.library).not.toBeNull();
+  });
 });
 
-test("should setup default library path", async () => {
-  const settings = fs.readJsonSync(path.join(resultDir, "settings.json"));
-  expect(settings.library).not.toBeNull();
+test.describe("with login", async () => {
+  let page: Page;
+
+  test.beforeAll(async () => {
+    const settings = fs.readJsonSync(path.join(resultDir, "settings.json"));
+    settings.user = user;
+    fs.writeJsonSync(path.join(resultDir, "settings.json"), settings);
+
+    page = await electronApp.firstWindow();
+    page.route("**/api/me", (route) => {
+      route.fulfill({
+        json: user,
+      });
+    });
+
+    await page.evaluate(() => {
+      return window.__ENJOY_APP__.app.reload();
+    });
+  });
+
+  test("should enter homepage after login", async () => {
+    await page.waitForSelector("[data-testid=layout-home]");
+
+    await page.screenshot({ path: "test-results/homepage.png" });
+
+    expect(await page.getByTestId("layout-onboarding").isVisible()).toBeFalsy();
+    expect(await page.getByTestId("layout-db-error").isVisible()).toBeFalsy();
+    expect(await page.getByTestId("layout-home").isVisible()).toBeTruthy();
+    expect(await page.getByTestId("sidebar").isVisible()).toBeTruthy();
+  });
 });
