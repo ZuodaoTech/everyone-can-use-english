fe2d1bbefbabe7d8757c12e28c6ab62a260beb2a
an-lee <an.lee.work@gmail.com>
2024-02-29 23:39:46 +0800

    update add-macos-cert script

--- enjoy/scripts/add-macos-cert.sh
+++ enjoy/scripts/add-macos-cert.sh
@@ -15,7 +15,7 @@ security default-keychain -s $KEY_CHAIN
 # Unlock the keychain
 security unlock-keychain -p actions $KEY_CHAIN
 
-security import $CERTIFICATE_P12 -k $KEY_CHAIN -P $MACOS_CERTIFICATE_PASSWORD -T /usr/bin/codesign;
+security import $CERTIFICATE_P12 -k $KEY_CHAIN -P $MACOS_CERTIFICATE_PASSWORD -T /usr/bin/codesign -T /usr/bin/productbuild;
 
 security set-key-partition-list -S apple-tool:,apple: -s -k actions $KEY_CHAIN
 
