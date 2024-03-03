d344984e3502e4ae7455c676151e573b1eae1c11
an-lee <an.lee.work@gmail.com>
2024-03-03 19:30:07 +0800

    update about

--- enjoy/src/renderer/components/preferences/about.tsx
+++ enjoy/src/renderer/components/preferences/about.tsx
@@ -1,5 +1,5 @@
 import { t } from "i18next";
-import { Button } from "@renderer/components/ui";
+import { Button, Separator } from "@renderer/components/ui";
 import { AppSettingsProviderContext } from "@renderer/context";
 import { useContext } from "react";
 
@@ -24,6 +24,52 @@ export const About = () => {
         >
           {t("checkUpdate")}
         </Button>
+      </div>
+
+      <Separator />
+
+      <div className="flex items-start justify-between py-4">
+        <div className="">
+          <div className="mb-2">{t("userGuide")}</div>
+        </div>
+        <Button
+          variant="secondary"
+          onClick={() => {
+            EnjoyApp.shell.openExternal("https://1000h.org/enjoy-app/");
+          }}
+        >
+          {t("open")}
+        </Button>
+      </div>
+
+      <Separator />
+
+      <div className="flex items-start justify-between py-4">
+        <div className="">
+          <div className="mb-2">{t("feedback")}</div>
+        </div>
+        <div className="flex items-center space-x-2">
+          <Button
+            variant="secondary"
+            onClick={() => {
+              EnjoyApp.shell.openExternal(
+                "https://mixin.one/codes/f8ff96b8-54fb-4ad8-a6d4-5a5bdb1df13e"
+              );
+            }}
+          >
+            Mixin
+          </Button>
+          <Button
+            variant="secondary"
+            onClick={() => {
+              EnjoyApp.shell.openExternal(
+                "https://github.com/xiaolai/everyone-can-use-english/discussions"
+              );
+            }}
+          >
+            GitHub
+          </Button>
+        </div>
       </div>
     </>
   );
