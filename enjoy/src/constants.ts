b7573a8f8a1099a29dbd499c6b1f9d0f8fd335f5
an-lee <an.lee.work@gmail.com>
2024-03-25 10:53:19 +0800

    add sentry for error tracking

--- enjoy/src/constants.ts
+++ enjoy/src/constants.ts
@@ -7,6 +7,8 @@ export const WEB_API_URL = "https://enjoy-web.fly.dev";
 
 export const REPO_URL = "https://github.com/xiaolai/everyone-can-use-english";
 
+export const SENTRY_DSN = "https://d51056d7af7d14eae446c0c15b4f3d31@o1168905.ingest.us.sentry.io/4506969353289728"
+
 export const MAGIC_TOKEN_REGEX =
   /\b(Mrs|Ms|Mr|Dr|Prof|St|[a-zA-Z]{1,2}|\d{1,2})\.\b/g;
 export const END_OF_SENTENCE_REGEX = /[^\.!,\?][\.!\?]/g;
