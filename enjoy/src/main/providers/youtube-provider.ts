c7ea43a8cb43914809404c8cfc92055e277c1e8b
HarryYC <sssbug@gmail.com>
2024-04-01 21:22:41 -0700

    Feat: add youtube provider

---
+++ enjoy/src/main/providers/youtube-provider.ts
@@ -0,0 +1,82 @@
+import log from "@main/logger";
+import $ from "cheerio";
+import { BrowserView, ipcMain } from "electron";
+
+const logger = log.scope("providers/youtube-provider");
+
+export class YoutubeProvider {
+  scrape = async (url: string) => {
+    return new Promise<string>((resolve, reject) => {
+      const view = new BrowserView();
+      view.webContents.loadURL(url);
+      logger.debug("started scraping", url);
+
+      view.webContents.on("did-finish-load", () => {
+        logger.debug("finished scraping", url);
+        view.webContents
+          .executeJavaScript(`document.documentElement.innerHTML`)
+          .then((html) => resolve(html as string))
+          .finally(() => {
+            (view.webContents as any).destroy();
+          });
+      });
+      view.webContents.on(
+        "did-fail-load",
+        (_event, _errorCode, error, validatedURL) => {
+          logger.error("failed scraping", url, error, validatedURL);
+          (view.webContents as any).destroy();
+          reject();
+        }
+      );
+    });
+  };
+
+  extractVideos = async (html: string) => {
+    try {
+      const json = $.load(html)("script")
+        .text()
+        .match(/ytInitialData = ({.*?});/)[1];
+      const data = JSON.parse(json);
+
+      const videoContents =
+        data.contents.twoColumnBrowseResultsRenderer.tabs[1].tabRenderer.content
+          .richGridRenderer.contents;
+
+      const videoList = videoContents
+        .filter((i: any) => i.richItemRenderer)
+        .map((video: any) => {
+          return {
+            title:
+              video.richItemRenderer.content.videoRenderer.title.runs[0].text,
+            thumbnail:
+              video.richItemRenderer.content.videoRenderer.thumbnail
+                .thumbnails[0].url,
+            videoId: video.richItemRenderer.content.videoRenderer.videoId,
+            duration:
+              video.richItemRenderer.content.videoRenderer.lengthText
+                .simpleText,
+          };
+        });
+
+      return videoList;
+    } catch (e) {
+      logger.error(e);
+      return [];
+    }
+  };
+
+  videos = async () => {
+    const html = await this.scrape("https://www.youtube.com/@CNN/videos");
+    return this.extractVideos(html);
+  };
+
+  registerIpcHandlers = () => {
+    ipcMain.handle("youtube-provider-videos", async () => {
+      try {
+        return await this.videos();
+      } catch (error) {
+        logger.error(error);
+      }
+    });
+  };
+}
