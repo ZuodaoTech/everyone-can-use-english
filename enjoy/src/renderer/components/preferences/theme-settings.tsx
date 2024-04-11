ac4a4cb4d95f9d050325700f80c665d36c5e3248
an-lee <an.lee.work@gmail.com>
2024-04-11 21:19:21 +0800

    fix warning

--- enjoy/src/renderer/components/preferences/theme-settings.tsx
+++ enjoy/src/renderer/components/preferences/theme-settings.tsx
@@ -29,7 +29,7 @@ export const ThemeSettings = () => {
             }}
           >
             <SelectTrigger className="text-xs">
-              <SelectValue asChild>
+              <SelectValue>
                 <Button variant="ghost" size="icon">
                   <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
                   <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
@@ -37,6 +37,7 @@ export const ThemeSettings = () => {
                 </Button>
               </SelectValue>
             </SelectTrigger>
+
             <SelectContent>
               <SelectItem className="text-xs" value="light">
                 {t("light")}
