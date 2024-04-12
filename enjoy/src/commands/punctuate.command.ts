7cc8e0bf7b507cee9433d169db3c6759e9c808a6
an-lee <an.lee.work@gmail.com>
2024-04-12 10:25:35 +0800

    use gpt-4-turbo as default (#515)

--- enjoy/src/commands/punctuate.command.ts
+++ enjoy/src/commands/punctuate.command.ts
@@ -11,7 +11,7 @@ export const punctuateCommand = async (
   }
 ): Promise<string> => {
   const { key, temperature = 0, baseUrl } = options;
-  let { modelName = "gpt-4-turbo-preview" } = options;
+  let { modelName = "gpt-4-turbo" } = options;
 
   const chatModel = new ChatOpenAI({
     openAIApiKey: key,
