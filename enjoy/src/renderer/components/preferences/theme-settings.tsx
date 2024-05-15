470dd5daf2ebe4e3fc40412587d1904428ec6140
an-lee <an.lee.work@gmail.com>
2024-05-15 18:38:58 +0800

    fix react warning

--- enjoy/src/renderer/components/preferences/theme-settings.tsx
+++ enjoy/src/renderer/components/preferences/theme-settings.tsx
@@ -30,10 +30,12 @@ export const ThemeSettings = () => {
           >
             <SelectTrigger className="text-xs">
               <SelectValue>
-                <Button variant="ghost" size="icon">
-                  <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-                  <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-                  <span className="sr-only">Toggle theme</span>
+                <Button asChild variant="ghost" size="icon">
+                  <a>
+                    <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                    <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                    <span className="sr-only">Toggle theme</span>
+                  </a>
                 </Button>
               </SelectValue>
             </SelectTrigger>
