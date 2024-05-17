76ccf05aff13e906b9c68c9f6b2cf6510574c785
an-lee <an.lee.work@gmail.com>
2024-05-17 22:10:24 +0800

    may delete & remove recording

--- enjoy/src/renderer/components/medias/media-transcription-read-button.tsx
+++ enjoy/src/renderer/components/medias/media-transcription-read-button.tsx
@@ -4,27 +4,83 @@ import {
 } from "@renderer/context";
 import { useContext, useEffect, useRef, useState } from "react";
 import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
   Button,
   Dialog,
   DialogContent,
   DialogTrigger,
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
   ScrollArea,
   toast,
 } from "@renderer/components/ui";
 import { TimelineEntry } from "echogarden/dist/utilities/Timeline";
 import { t } from "i18next";
 import WaveSurfer from "wavesurfer.js";
-import { LoaderIcon, MicIcon, SquareIcon } from "lucide-react";
+import {
+  DownloadIcon,
+  LoaderIcon,
+  MicIcon,
+  MoreHorizontalIcon,
+  SquareIcon,
+  Trash2Icon,
+} from "lucide-react";
 import RecordPlugin from "wavesurfer.js/dist/plugins/record";
 import { useRecordings } from "@/renderer/hooks";
 import { formatDateTime } from "@/renderer/lib/utils";
+import { MediaPlayer, MediaProvider } from "@vidstack/react";
+import {
+  DefaultAudioLayout,
+  defaultLayoutIcons,
+} from "@vidstack/react/player/layouts/default";
 
 const TEN_MINUTES = 60 * 10;
 let interval: NodeJS.Timeout;
 
 export const MediaTranscriptionReadButton = () => {
   const [open, setOpen] = useState(false);
+  const [selectedRecording, setSelectedRecording] = useState(null);
+  const { EnjoyApp } = useContext(AppSettingsProviderContext);
   const { media, transcription } = useContext(MediaPlayerProviderContext);
+
+  const handleDelete = () => {
+    if (!selectedRecording) return;
+
+    EnjoyApp.recordings.destroy(selectedRecording.id);
+  };
+
+  const handleDownload = (recording: RecordingType) => {
+    EnjoyApp.dialog
+      .showSaveDialog({
+        title: t("download"),
+        defaultPath: recording.filename,
+      })
+      .then((savePath) => {
+        if (!savePath) return;
+
+        toast.promise(
+          EnjoyApp.download.start(recording.src, savePath as string),
+          {
+            loading: t("downloading", { file: recording.filename }),
+            success: () => t("downloadedSuccessfully"),
+            error: t("downloadFailed"),
+            position: "bottom-right",
+          }
+        );
+      })
+      .catch((err) => {
+        if (err) toast.error(err.message);
+      });
+  };
 
   const {
     recordings,
@@ -34,51 +90,109 @@ export const MediaTranscriptionReadButton = () => {
   } = useRecordings(media, -1);
 
   return (
-    <Dialog open={open} onOpenChange={setOpen}>
-      <DialogTrigger asChild>
-        <Button variant="outline" size="sm" className="hidden lg:block">
-          {t("readThrough")}
-        </Button>
-      </DialogTrigger>
-      <DialogContent className="max-w-screen-md xl:max-w-screen-lg h-5/6 flex flex-col p-0">
-        <ScrollArea className="flex-1 px-6 pt-4">
-          <div className="select-text mx-auto prose xl:prose-lg dark:prose-revert">
-            <h3>{media.name}</h3>
-            {transcription.result.timeline.map((sentence: TimelineEntry) => (
-              <p>{sentence.text}</p>
-            ))}
-          </div>
-          <div className="mt-12">
-            {recordings.map((recording) => (
-              <div className="w-full mb-4" id={recording.id}>
-                <div className="text-sm text-muted-foreground mb-2 text-right">
-                  {formatDateTime(recording.createdAt)}
-                </div>
-                <audio className="w-full" controls>
-                  <source src={recording.src} type="audio/wav" />
-                </audio>
-              </div>
-            ))}
-            {hasMoreRecordings && (
-              <div className="flex items-center justify-center">
-                <Button
-                  variant="secondary"
-                  onClick={() => fetchRecordings(recordings.length)}
+    <>
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogTrigger asChild>
+          <Button variant="outline" size="sm" className="hidden lg:block">
+            {t("readThrough")}
+          </Button>
+        </DialogTrigger>
+        <DialogContent className="max-w-screen-md xl:max-w-screen-lg h-5/6 flex flex-col p-0">
+          <ScrollArea className="flex-1 px-6 pt-4">
+            <div className="select-text mx-auto w-full max-w-prose">
+              <h3 className="font-bold text-xl my-4">{media.name}</h3>
+              {transcription.result.timeline.map(
+                (sentence: TimelineEntry, index: number) => (
+                  <div key={index} className="flex flex-start space-x-2 mb-4">
+                    <span className="text-sm text-muted-foreground min-w-max leading-8">#{index + 1}</span>
+                    <div className="text-lg leading-8">{sentence.text}</div>
+                  </div>
+                )
+              )}
+            </div>
+            <div className="mt-12">
+              {recordings.map((recording) => (
+                <div
+                  className="mx-auto w-full max-w-prose px-4 mb-4"
+                  id={recording.id}
                 >
-                  {loadingRecordings && (
-                    <LoaderIcon className="w-4 h-4 animate-spin" />
-                  )}
-                  <span>{t("loadMore")}</span>
-                </Button>
-              </div>
-            )}
+                  <div className="flex items-center justify-end space-x-2 mb-2">
+                    <span className="text-sm text-muted-foreground">
+                      {formatDateTime(recording.createdAt)}
+                    </span>
+                    <DropdownMenu>
+                      <DropdownMenuTrigger>
+                        <MoreHorizontalIcon className="w-4 h-4" />
+                      </DropdownMenuTrigger>
+
+                      <DropdownMenuContent>
+                        <DropdownMenuItem
+                          className="cursor-pointer"
+                          onClick={() => handleDownload(recording)}
+                        >
+                          <DownloadIcon className="w-4 h-4 mr-2" />
+                          <span>{t("download")}</span>
+                        </DropdownMenuItem>
+                        <DropdownMenuItem
+                          className="text-destructive cursor-pointer"
+                          onClick={() => setSelectedRecording(recording)}
+                        >
+                          <Trash2Icon className="w-4 h-4 mr-2" />
+                          <span>{t("delete")}</span>
+                        </DropdownMenuItem>
+                      </DropdownMenuContent>
+                    </DropdownMenu>
+                  </div>
+                  <MediaPlayer viewType="audio" src={recording.src}>
+                    <MediaProvider />
+                    <DefaultAudioLayout icons={defaultLayoutIcons} />
+                  </MediaPlayer>
+                </div>
+              ))}
+              {hasMoreRecordings && (
+                <div className="flex items-center justify-center">
+                  <Button
+                    variant="secondary"
+                    onClick={() => fetchRecordings(recordings.length)}
+                  >
+                    {loadingRecordings && (
+                      <LoaderIcon className="w-4 h-4 animate-spin" />
+                    )}
+                    <span>{t("loadMore")}</span>
+                  </Button>
+                </div>
+              )}
+            </div>
+          </ScrollArea>
+          <div className="h-16 border-t">
+            {open && <RecorderButton onRecorded={() => fetchRecordings(0)} />}
           </div>
-        </ScrollArea>
-        <div className="h-16">
-          {open && <RecorderButton onRecorded={() => fetchRecordings(0)} />}
-        </div>
-      </DialogContent>
-    </Dialog>
+        </DialogContent>
+      </Dialog>
+
+      <AlertDialog
+        open={selectedRecording}
+        onOpenChange={(value) => {
+          if (value) return;
+          setSelectedRecording(null);
+        }}
+      >
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>{t("deleteRecording")}</AlertDialogTitle>
+            <AlertDialogDescription>
+              {t("deleteRecordingConfirmation")}
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>{t("cancel")}</AlertDialogCancel>
+            <AlertDialogAction asChild>
+              <Button onClick={handleDelete}>{t("delete")}</Button>
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </>
   );
 };
 
@@ -162,7 +276,6 @@ const RecorderButton = (props: { onRecorded: () => void }) => {
   };
 
   useEffect(() => {
-    console.log(access, ref?.current);
     if (!access) return;
     if (!ref?.current) return;
 
