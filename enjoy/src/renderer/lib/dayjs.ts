9b66d2ddc4046cd69d8e1ec1be022394124bc4c7
an-lee <an.lee.work@gmail.com>
2024-05-17 10:14:42 +0800

    refactor code

--- enjoy/src/renderer/lib/dayjs.ts
+++ enjoy/src/renderer/lib/dayjs.ts
@@ -4,23 +4,19 @@ import "dayjs/locale/zh-cn";
 import utc from "dayjs/plugin/utc";
 import timezone from "dayjs/plugin/timezone";
 import localeData from "dayjs/plugin/localeData";
-
 import localizedFormat from "dayjs/plugin/localizedFormat";
 import relativeTime from "dayjs/plugin/relativeTime";
 import duration from "dayjs/plugin/duration";
-import "dayjs/locale/en";
-import "dayjs/locale/zh-cn";
 import i18next from "i18next";
 
 dayjs.extend(localizedFormat);
 dayjs.extend(duration);
 dayjs.extend(relativeTime);
-
 dayjs.extend(localeData);
-dayjs.locale(i18next.resolvedLanguage?.toLowerCase() || "en");
 dayjs.extend(utc);
 dayjs.extend(timezone);
 
+dayjs.locale(i18next.resolvedLanguage?.toLowerCase() || "en");
 try {
   dayjs.tz.setDefault(Intl.DateTimeFormat().resolvedOptions().timeZone);
 } catch (e) {
