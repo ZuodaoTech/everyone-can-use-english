46a7234a75a0a5f1b1a332eb53c95ea5362040fb
an-lee <an.lee.work@gmail.com>
2024-06-11 11:24:21 +0800

    update style

--- enjoy/src/renderer/components/pronunciation-assessments/pronunciation-assessment-card.tsx
+++ enjoy/src/renderer/components/pronunciation-assessments/pronunciation-assessment-card.tsx
@@ -1,6 +1,7 @@
-import { RadialProgress } from "@renderer/components/ui";
+import { Button, RadialProgress } from "@renderer/components/ui";
 import { scoreColor } from "./pronunciation-assessment-score-result";
 import { t } from "i18next";
+import { formatDateTime } from "@/renderer/lib/utils";
 
 export const PronunciationAssessmentCard = (props: {
   pronunciationAssessment: PronunciationAssessmentType;
@@ -12,11 +13,11 @@ export const PronunciationAssessmentCard = (props: {
       className="bg-background p-4 rounded-lg border hover:shadow"
     >
       <div className="flex items-start space-x-4">
-        <div className="flex-1">
+        <div className="flex-1 flex flex-col h-32">
           <div className="select-text line-clamp-2 text-muted-foreground font-serif pl-3 border-l-4 mb-4">
             {assessment.referenceText || assessment.target.referenceText}
           </div>
-          <div className="flex items-center gap-4 flex-wrap">
+          <div className="flex items-center gap-2 flex-wrap mb-4">
             {assessment.accuracyScore && (
               <div className="flex items-center space-x-2 mb-2">
                 <span className="text-muted-foreground text-sm">
@@ -116,14 +117,22 @@ export const PronunciationAssessmentCard = (props: {
               </div>
             )}
           </div>
+          <div className="mt-auto">
+            <div className="text-xs text-muted-foreground">{formatDateTime(assessment.createdAt)}</div>
+          </div>
         </div>
-        <div className="h-24">
+        <div className="h-32">
           <RadialProgress
-            className="w-24 h-24 mx-auto"
+            className="w-20 h-20 mx-auto mb-2"
             ringClassName={`${scoreColor(assessment.pronunciationScore || 0)}`}
             progress={assessment.pronunciationScore || 0}
             fontSize={24}
           />
+          <div className="flex justify-center">
+            <Button variant="secondary" size="sm">
+              {t("detail")}
+            </Button>
+          </div>
         </div>
       </div>
     </div>
