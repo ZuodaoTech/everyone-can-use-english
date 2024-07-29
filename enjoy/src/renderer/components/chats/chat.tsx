ae408500782e4d2e014e815d71e591b1c242af0d
an-lee <an.lee.work@gmail.com>
2024-07-30 06:34:58 +0800

    add vad

--- enjoy/src/renderer/components/chats/chat.tsx
+++ enjoy/src/renderer/components/chats/chat.tsx
@@ -1,4 +1,4 @@
-import { MicIcon, SettingsIcon } from "lucide-react";
+import { MicIcon, SettingsIcon, SquareIcon } from "lucide-react";
 import {
   Button,
   Dialog,
@@ -8,14 +8,36 @@ import {
   ScrollArea,
 } from "@renderer/components/ui";
 import { t } from "i18next";
-import { ChatProviderContext } from "@renderer/context";
-import { useContext, useState } from "react";
+import {
+  AppSettingsProviderContext,
+  ChatProviderContext,
+} from "@renderer/context";
+import { useContext, useEffect, useState } from "react";
 import { ChatForm } from "./chat-form";
+import { useMicVAD } from "@ricky0123/vad-react";
 
 export const Chat = () => {
   const { currentChat, chatAgents, updateChat, destroyChat } =
     useContext(ChatProviderContext);
   const [displayChatForm, setDisplayChatForm] = useState(false);
+  const { EnjoyApp } = useContext(AppSettingsProviderContext);
+
+  const askForMediaAccess = () => {
+    EnjoyApp.system.preferences.mediaAccess("microphone");
+  };
+
+  const vad = useMicVAD({
+    workletURL: "./vad.worklet.bundle.min.js", // setting workletURL
+    modelURL: "./silero_vad.onnx", // setting modelURL
+    startOnLoad: false,
+    onSpeechEnd: (audio) => {
+      console.log("Speech end detected");
+      // do something with `audio` (Float32Array of audio samples at sample rate 16000)...
+      const audioArray = new Float32Array(audio); // Example audio data
+      vad.pause();
+      return audioArray;
+    },
+  });
 
   if (!currentChat) {
     return (
@@ -59,12 +81,23 @@ export const Chat = () => {
       <div className="flex-1 space-y-4 px-4 mb-4"></div>
       <div className="absolute bottom-0 w-full h-16 border-t z-10 bg-background flex items-center">
         <div className="w-full flex justify-center">
-          <Button
-            className="rounded-full bg-red-500 hover:bg-red-600 shadow w-10 h-10"
-            size="icon"
-          >
-            <MicIcon className="w-6 h-6" />
-          </Button>
+          {vad.listening ? (
+            <Button
+              onClick={() => vad.pause()}
+              className="rounded-full bg-red-500 hover:bg-red-600 shadow w-10 h-10"
+              size="icon"
+            >
+              <SquareIcon fill="white" className="w-6 h-6 text-white" />
+            </Button>
+          ) : (
+            <Button
+              onClick={() => vad.start()}
+              className="rounded-full bg-red-500 hover:bg-red-600 shadow w-10 h-10"
+              size="icon"
+            >
+              <MicIcon className="w-6 h-6" />
+            </Button>
+          )}
         </div>
       </div>
     </ScrollArea>
--- enjoy/vite.renderer.config.ts
+++ enjoy/vite.renderer.config.ts
@@ -27,6 +27,18 @@ export default defineConfig((env) => {
             src: "assets/*",
             dest: "assets",
           },
+          {
+            src: "node_modules/@ricky0123/vad-web/dist/vad.worklet.bundle.min.js",
+            dest: "./",
+          },
+          {
+            src: "node_modules/@ricky0123/vad-web/dist/silero_vad.onnx",
+            dest: "./",
+          },
+          {
+            src: "node_modules/onnxruntime-web/dist/*.wasm",
+            dest: "./",
+          },
         ],
       }),
     ],
