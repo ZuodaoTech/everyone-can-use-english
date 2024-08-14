102bd5b5d4897e17143cca9327b4f04bebdea655
an-lee <an.lee.work@gmail.com>
2024-08-14 17:55:45 +0800

    remove deprecated codes

--- enjoy/src/renderer/components/medias/index.ts
+++ enjoy/src/renderer/components/medias/index.ts
@@ -3,7 +3,6 @@ export * from "./media-caption";
 export * from "./media-info-panel";
 export * from "./media-recordings";
 export * from "./media-current-recording";
-export * from "./media-recorder";
 export * from "./media-transcription";
 export * from "./media-transcription-read-button";
 export * from "./media-transcription-generate-button";
