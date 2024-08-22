ffbfc8c60722e92685615bb8f4bb5ebb903f859d
an-lee <an.lee.work@gmail.com>
2024-08-22 16:52:12 +0800

    update style

--- enjoy/src/renderer/components/chats/chat-user-message.tsx
+++ enjoy/src/renderer/components/chats/chat-user-message.tsx
@@ -24,6 +24,7 @@ import { t } from "i18next";
 import {
   CheckIcon,
   ChevronDownIcon,
+  ChevronRightIcon,
   ChevronUpIcon,
   CopyIcon,
   DownloadIcon,
@@ -47,6 +48,103 @@ import { useCopyToClipboard } from "@uidotdev/usehooks";
 
 export const ChatUserMessage = (props: { chatMessage: ChatMessageType }) => {
   const { chatMessage } = props;
+  const { recording } = chatMessage;
+  const ref = useRef<HTMLDivElement>(null);
+  const [editing, setEditing] = useState<boolean>(false);
+  const [content, setContent] = useState<string>(chatMessage.content);
+  const { onUpdateMessage } = useContext(ChatSessionProviderContext);
+
+  useEffect(() => {
+    if (ref.current) {
+      ref.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [ref]);
+
+  return (
+    <div ref={ref} className="mb-6">
+      <div className="flex items-center space-x-2 justify-end mb-2">
+        <div className="text-sm text-muted-foreground">
+          {chatMessage.member.user.name}
+        </div>
+        <Avatar className="w-8 h-8 bg-background avatar">
+          <AvatarImage src={chatMessage.member.user.avatarUrl}></AvatarImage>
+          <AvatarFallback className="bg-background">
+            {chatMessage.member.user.name}
+          </AvatarFallback>
+        </Avatar>
+      </div>
+      <div className="flex justify-end">
+        <div className="flex flex-col gap-2 p-4 mb-2 bg-sky-500/30 border-sky-500 rounded-lg shadow-sm w-full max-w-prose">
+          {recording && (
+            <WavesurferPlayer id={recording.id} src={recording.src} />
+          )}
+          {recording?.pronunciationAssessment && (
+            <div className="flex justify-end">
+              <PronunciationAssessmentScoreDetail
+                assessment={recording.pronunciationAssessment}
+              />
+            </div>
+          )}
+          {editing ? (
+            <div className="">
+              <Textarea
+                className="bg-background mb-2"
+                value={content}
+                onChange={(event) => setContent(event.target.value)}
+              />
+              <div className="flex justify-end space-x-4">
+                <Button
+                  onClick={() => setEditing(false)}
+                  variant="secondary"
+                  size="sm"
+                >
+                  {t("cancel")}
+                </Button>
+                <Button
+                  onClick={() =>
+                    onUpdateMessage(chatMessage.id, { content }).finally(() =>
+                      setEditing(false)
+                    )
+                  }
+                  variant="default"
+                  size="sm"
+                >
+                  {t("save")}
+                </Button>
+              </div>
+            </div>
+          ) : (
+            <MarkdownWrapper className="select-text prose dark:prose-invert">
+              {chatMessage.content}
+            </MarkdownWrapper>
+          )}
+          <ChatUserMessageActions
+            chatMessage={chatMessage}
+            setContent={setContent}
+            setEditing={setEditing}
+          />
+        </div>
+      </div>
+      <div className="flex justify-end text-xs text-muted-foreground timestamp">
+        {formatDateTime(chatMessage.createdAt)}
+      </div>
+    </div>
+  );
+};
+
+const ChatUserMessageActions = (props: {
+  chatMessage: ChatMessageType;
+  setContent: (content: string) => void;
+  setEditing: (value: boolean) => void;
+}) => {
+  const { chatMessage, setContent, setEditing } = props;
+  const { recording } = chatMessage;
+  const [refinement, setRefinement] = useState<string>();
+  const [refining, setRefining] = useState<boolean>(false);
+  const [refinementVisible, setRefinementVisible] = useState<boolean>(true);
+  const { refine } = useAiCommand();
+  const [_, copyToClipboard] = useCopyToClipboard();
+  const [copied, setCopied] = useState<boolean>(false);
   const { EnjoyApp } = useContext(AppSettingsProviderContext);
   const {
     chatMessages,
@@ -55,22 +153,11 @@ export const ChatUserMessage = (props: { chatMessage: ChatMessageType }) => {
     isPaused,
     setAssessing,
     onDeleteMessage,
-    onUpdateMessage,
     submitting,
   } = useContext(ChatSessionProviderContext);
   const { currentChat } = useContext(ChatProviderContext);
-  const { recording } = chatMessage;
-  const ref = useRef<HTMLDivElement>(null);
-  const [refinement, setRefinement] = useState<string>();
-  const [refining, setRefining] = useState<boolean>(false);
-  const [refinementVisible, setRefinementVisible] = useState<boolean>(true);
-  const [editing, setEditing] = useState<boolean>(false);
-  const [content, setContent] = useState<string>(chatMessage.content);
-  const { refine } = useAiCommand();
-  const [_, copyToClipboard] = useCopyToClipboard();
-  const [copied, setCopied] = useState<boolean>(false);
 
-  const handleSuggest = async (params?: { reload?: boolean }) => {
+  const handleRefine = async (params?: { reload?: boolean }) => {
     if (refining) return;
     if (!chatMessage.content) return;
 
@@ -147,200 +234,138 @@ export const ChatUserMessage = (props: { chatMessage: ChatMessageType }) => {
         if (err) toast.error(err.message);
       });
   };
-
-  useEffect(() => {
-    if (ref.current) {
-      ref.current.scrollIntoView({ behavior: "smooth" });
-    }
-  }, [ref]);
-
   return (
-    <div ref={ref} className="mb-6">
-      <div className="flex items-center space-x-2 justify-end mb-2">
-        <div className="text-sm text-muted-foreground">
-          {chatMessage.member.user.name}
-        </div>
-        <Avatar className="w-8 h-8 bg-background avatar">
-          <AvatarImage src={chatMessage.member.user.avatarUrl}></AvatarImage>
-          <AvatarFallback className="bg-background">
-            {chatMessage.member.user.name}
-          </AvatarFallback>
-        </Avatar>
-      </div>
-      <div className="flex justify-end">
-        <div className="flex flex-col gap-2 px-4 py-2 mb-2 bg-sky-500/30 border-sky-500 rounded-lg shadow-sm w-full max-w-prose">
-          {recording && (
-            <WavesurferPlayer id={recording.id} src={recording.src} />
-          )}
-          {recording?.pronunciationAssessment && (
-            <div className="flex justify-end">
-              <PronunciationAssessmentScoreDetail
-                assessment={recording.pronunciationAssessment}
+    <>
+      <DropdownMenu>
+        <div className="flex items-center justify-end space-x-4">
+          {chatMessage.state === "pending" && (
+            <>
+              <EditIcon
+                data-tooltip-id="global-tooltip"
+                data-tooltip-content={t("edit")}
+                className="w-4 h-4 cursor-pointer"
+                onClick={() => {
+                  setContent(chatMessage.content);
+                  setEditing(true);
+                }}
               />
-            </div>
-          )}
-          {editing ? (
-            <div className="">
-              <Textarea
-                className="bg-background mb-2"
-                value={content}
-                onChange={(event) => setContent(event.target.value)}
-              />
-              <div className="flex justify-end space-x-4">
-                <Button
-                  onClick={() => setEditing(false)}
-                  variant="secondary"
-                  size="sm"
-                >
-                  {t("cancel")}
-                </Button>
-                <Button
-                  onClick={() =>
-                    onUpdateMessage(chatMessage.id, { content }).finally(() =>
-                      setEditing(false)
-                    )
-                  }
-                  variant="default"
-                  size="sm"
-                >
-                  {t("save")}
-                </Button>
-              </div>
-            </div>
-          ) : (
-            <MarkdownWrapper className="select-text prose dark:prose-invert">
-              {chatMessage.content}
-            </MarkdownWrapper>
-          )}
-          {refinement && (
-            <Collapsible
-              open={refinementVisible}
-              onOpenChange={(value) => setRefinementVisible(value)}
-            >
-              <CollapsibleContent>
-                <div className="p-4 font-serif bg-background rounded">
-                  <MarkdownWrapper className="select-text prose dark:prose-invert">
-                    {refinement}
-                  </MarkdownWrapper>
-                </div>
-              </CollapsibleContent>
-              <div className="my-2 flex justify-center">
-                <CollapsibleTrigger asChild>
-                  <Button
-                    onClick={() => setRefinementVisible(!refinementVisible)}
-                    variant="ghost"
-                    size="icon"
-                    className="w-6 h-6"
-                  >
-                    {refinementVisible ? (
-                      <ChevronUpIcon className="w-4 h-4" />
-                    ) : (
-                      <ChevronDownIcon className="w-4 h-4" />
-                    )}
-                  </Button>
-                </CollapsibleTrigger>
-              </div>
-            </Collapsible>
-          )}
-          <DropdownMenu>
-            <div className="flex items-center justify-end space-x-4">
-              {chatMessage.state === "pending" && (
-                <>
-                  <EditIcon
-                    data-tooltip-id="global-tooltip"
-                    data-tooltip-content={t("edit")}
-                    className="w-4 h-4 cursor-pointer"
-                    onClick={() => {
-                      setContent(chatMessage.content);
-                      setEditing(true);
-                    }}
-                  />
-                  {isPaused || isRecording || submitting ? (
-                    <LoaderIcon className="w-4 h-4 animate-spin" />
-                  ) : (
-                    <MicIcon
-                      data-tooltip-id="global-tooltip"
-                      data-tooltip-content={t("reRecord")}
-                      className="w-4 h-4 cursor-pointer"
-                      onClick={startRecording}
-                    />
-                  )}
-                </>
-              )}
-              {chatMessage.recording && (
-                <GaugeCircleIcon
-                  data-tooltip-id="global-tooltip"
-                  data-tooltip-content={t("pronunciationAssessment")}
-                  onClick={() => setAssessing(recording)}
-                  className="w-4 h-4 cursor-pointer"
-                />
-              )}
-              {refining ? (
+              {isPaused || isRecording || submitting ? (
                 <LoaderIcon className="w-4 h-4 animate-spin" />
               ) : (
-                <SparklesIcon
+                <MicIcon
                   data-tooltip-id="global-tooltip"
-                  data-tooltip-content={t("refine")}
+                  data-tooltip-content={t("reRecord")}
                   className="w-4 h-4 cursor-pointer"
-                  onClick={() => handleSuggest()}
+                  onClick={startRecording}
                 />
               )}
-              {copied ? (
-                <CheckIcon className="w-4 h-4 text-green-500" />
-              ) : (
-                <CopyIcon
-                  data-tooltip-id="global-tooltip"
-                  data-tooltip-content={t("copyText")}
-                  className="w-4 h-4 cursor-pointer"
-                  onClick={() => {
-                    copyToClipboard(chatMessage.content);
-                    setCopied(true);
-                    setTimeout(() => {
-                      setCopied(false);
-                    }, 3000);
-                  }}
-                />
-              )}
-              <ConversationShortcuts
-                prompt={chatMessage.content}
-                excludedIds={[]}
-                trigger={
-                  <ForwardIcon
-                    data-tooltip-id="global-tooltip"
-                    data-tooltip-content={t("forward")}
-                    className="w-4 h-4 cursor-pointer"
-                  />
-                }
+            </>
+          )}
+          {chatMessage.recording && (
+            <GaugeCircleIcon
+              data-tooltip-id="global-tooltip"
+              data-tooltip-content={t("pronunciationAssessment")}
+              onClick={() => setAssessing(recording)}
+              className="w-4 h-4 cursor-pointer"
+            />
+          )}
+          {refining ? (
+            <LoaderIcon className="w-4 h-4 animate-spin" />
+          ) : (
+            <SparklesIcon
+              data-tooltip-id="global-tooltip"
+              data-tooltip-content={t("refine")}
+              className="w-4 h-4 cursor-pointer"
+              onClick={() => handleRefine()}
+            />
+          )}
+          {copied ? (
+            <CheckIcon className="w-4 h-4 text-green-500" />
+          ) : (
+            <CopyIcon
+              data-tooltip-id="global-tooltip"
+              data-tooltip-content={t("copyText")}
+              className="w-4 h-4 cursor-pointer"
+              onClick={() => {
+                copyToClipboard(chatMessage.content);
+                setCopied(true);
+                setTimeout(() => {
+                  setCopied(false);
+                }, 3000);
+              }}
+            />
+          )}
+          <ConversationShortcuts
+            prompt={chatMessage.content}
+            excludedIds={[]}
+            trigger={
+              <ForwardIcon
+                data-tooltip-id="global-tooltip"
+                data-tooltip-content={t("forward")}
+                className="w-4 h-4 cursor-pointer"
               />
-              {Boolean(chatMessage.recording) && (
-                <DownloadIcon
-                  data-tooltip-id="global-tooltip"
-                  data-tooltip-content={t("download")}
-                  data-testid="chat-message-download-recording"
-                  onClick={handleDownload}
-                  className="w-4 h-4 cursor-pointer"
-                />
-              )}
-              <DropdownMenuTrigger>
-                <MoreVerticalIcon className="w-4 h-4" />
-              </DropdownMenuTrigger>
-            </div>
-            <DropdownMenuContent>
-              <DropdownMenuItem
-                className="cursor-pointer"
-                onClick={() => onDeleteMessage(chatMessage.id)}
-              >
-                <span className="mr-auto text-destructive capitalize">
-                  {t("delete")}
-                </span>
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
+            }
+          />
+          {Boolean(chatMessage.recording) && (
+            <DownloadIcon
+              data-tooltip-id="global-tooltip"
+              data-tooltip-content={t("download")}
+              data-testid="chat-message-download-recording"
+              onClick={handleDownload}
+              className="w-4 h-4 cursor-pointer"
+            />
+          )}
+          <DropdownMenuTrigger>
+            <MoreVerticalIcon className="w-4 h-4" />
+          </DropdownMenuTrigger>
         </div>
-      </div>
-      <div className="flex justify-end text-xs text-muted-foreground timestamp">
-        {formatDateTime(chatMessage.createdAt)}
-      </div>
-    </div>
+        <DropdownMenuContent>
+          <DropdownMenuItem
+            className="cursor-pointer"
+            onClick={() => onDeleteMessage(chatMessage.id)}
+          >
+            <span className="mr-auto text-destructive capitalize">
+              {t("delete")}
+            </span>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      {refinement && (
+        <div className="w-full bg-background rounded mt-4">
+          <Collapsible
+            open={refinementVisible}
+            onOpenChange={(value) => setRefinementVisible(value)}
+          >
+            <CollapsibleTrigger asChild>
+              <div className="flex items-center justify-between px-4 py-2 cursor-pointer">
+                <div className="flex items-center space-x-2">
+                  <SparklesIcon className="w-4 h-4" />
+                  <span className="text-sm">{t("refine")}</span>
+                </div>
+                <Button
+                  onClick={() => setRefinementVisible(!refinementVisible)}
+                  variant="ghost"
+                  size="icon"
+                  className="w-6 h-6"
+                >
+                  {refinementVisible ? (
+                    <ChevronRightIcon className="w-4 h-4" />
+                  ) : (
+                    <ChevronDownIcon className="w-4 h-4" />
+                  )}
+                </Button>
+              </div>
+            </CollapsibleTrigger>
+            <CollapsibleContent>
+              <div className="p-4 font-serif border-t">
+                <MarkdownWrapper className="select-text prose dark:prose-invert">
+                  {refinement}
+                </MarkdownWrapper>
+              </div>
+            </CollapsibleContent>
+          </Collapsible>
+        </div>
+      )}
+    </>
   );
 };
