33a780cdd1146bc545234d6fe4935037dad46ad3
an-lee <an.lee.work@gmail.com>
2024-10-07 17:16:02 +0800

    audo backup db file when started

--- enjoy/src/main/db/index.ts
+++ enjoy/src/main/db/index.ts
@@ -44,6 +44,7 @@ import url from "url";
 import { i18n } from "@main/i18n";
 import { UserSettingKeyEnum } from "@/types/enums";
 import log from "@main/logger";
+import fs from "fs-extra";
 
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -55,6 +56,8 @@ const db = {
   disconnect: async () => {},
   registerIpcHandlers: () => {},
   isConnecting: false,
+  backup: async () => {},
+  restore: async (backupFilePath: string) => {},
 };
 
 const handlers = [
@@ -164,6 +167,8 @@ db.connect = async () => {
     });
 
     try {
+      await db.backup();
+
       // migrate up to the latest state
       await umzug.up();
 
@@ -216,6 +221,73 @@ db.disconnect = async () => {
   db.connection = null;
 };
 
+db.backup = async () => {
+  const dbPath = settings.dbPath();
+  if (!dbPath) {
+    logger.error("Db path is not ready");
+    return;
+  }
+
+  const backupPath = path.join(settings.userDataPath(), "backup");
+  fs.ensureDirSync(backupPath);
+
+  // Check if the last backup is older than 1 day
+  const lastBackup = fs
+    .readdirSync(backupPath)
+    .filter((file) => file.startsWith(path.basename(dbPath)))
+    .sort()
+    .pop();
+  if (
+    lastBackup &&
+    new Date(lastBackup.split(".")[lastBackup.split(".").length - 1]) >
+      new Date(Date.now() - 1000 * 60 * 60 * 24)
+  ) {
+    logger.info("Backup is up to date");
+    return;
+  }
+
+  // Only keep the latest 10 backups
+  const backupFiles = fs
+    .readdirSync(backupPath)
+    .filter((file) => file.startsWith(path.basename(dbPath)));
+  if (backupFiles.length >= 10) {
+    fs.unlinkSync(path.join(backupPath, backupFiles[0]));
+  }
+
+  const backupFilePath = path.join(
+    backupPath,
+    `${path.basename(dbPath)}.${Date.now()}`
+  );
+  fs.copySync(dbPath, backupFilePath);
+
+  logger.info(`Backup created at ${backupFilePath}`);
+};
+
+db.restore = async (backupFilePath: string) => {
+  const dbPath = settings.dbPath();
+  if (!dbPath) {
+    logger.error("Db path is not ready");
+    return;
+  }
+
+  if (!fs.existsSync(backupFilePath)) {
+    logger.error(`Backup file not found at ${backupFilePath}`);
+    return;
+  }
+
+  try {
+    await db.disconnect();
+
+    fs.copySync(backupFilePath, dbPath);
+    logger.info(`Database restored from ${backupFilePath}`);
+  } catch (err) {
+    logger.error(err);
+    throw err;
+  } finally {
+    db.connect();
+  }
+};
+
 db.registerIpcHandlers = () => {
   ipcMain.handle("db-connect", async () => {
     if (db.isConnecting)
@@ -243,6 +315,14 @@ db.registerIpcHandlers = () => {
 
   ipcMain.handle("db-disconnect", async () => {
     db.disconnect();
+  });
+
+  ipcMain.handle("db-backup", async () => {
+    db.backup();
+  });
+
+  ipcMain.handle("db-restore", async (_, backupFilePath: string) => {
+    db.restore(backupFilePath);
   });
 };
 
