cadbddaa4990b2b2dd2dfd2461f58603838201b7
an-lee <an.lee.work@gmail.com>
2024-10-10 09:18:32 +0800

    remove unused deps

--- enjoy/vite.renderer.config.ts
+++ enjoy/vite.renderer.config.ts
@@ -27,18 +27,6 @@ export default defineConfig((env) => {
             src: "assets/*",
             dest: "assets",
           },
-          {
-            src: "node_modules/@ricky0123/vad-web/dist/vad.worklet.bundle.min.js",
-            dest: "./",
-          },
-          {
-            src: "node_modules/@ricky0123/vad-web/dist/silero_vad.onnx",
-            dest: "./",
-          },
-          {
-            src: "node_modules/onnxruntime-web/dist/*.wasm",
-            dest: "./",
-          },
         ],
       }),
     ],
