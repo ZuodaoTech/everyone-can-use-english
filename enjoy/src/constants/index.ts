46d3f73614a65bae8fa0cd4c760c2d0335c5491a
an-lee <an.lee.work@gmail.com>
2024-10-22 06:45:13 +0800

    fix constant download url

--- enjoy/src/constants/index.ts
+++ enjoy/src/constants/index.ts
@@ -34,7 +34,7 @@ export const AI_WORKER_ENDPOINT = "https://ai-worker.enjoy.bot";
 export const WEB_API_URL = "https://enjoy.bot";
 export const WS_URL = "wss://enjoy.bot";
 
-export const DOWNLOAD_URL = "https://1000h.org/enjoy-app/download.html";
+export const DOWNLOAD_URL = "https://1000h.org/enjoy-app/install.html";
 
 export const REPO_URL =
   "https://github.com/zuodaotech/everyone-can-use-english";
