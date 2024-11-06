e76f3e3b220f602409a338ba7bc479d74631bdbd
an-lee <an.lee.work@gmail.com>
2024-11-06 09:42:10 +0800

    refactor

--- enjoy/src/renderer/components/widgets/sentence.tsx
+++ enjoy/src/renderer/components/widgets/sentence.tsx
@@ -1,30 +1,27 @@
+import { memo } from "react";
 import { Vocabulary } from "@renderer/components";
 import { cn } from "@renderer/lib/utils";
 
-export const Sentence = ({
-  sentence,
-  className,
-}: {
-  sentence: string;
-  className?: string;
-}) => {
-  // split by space or punctuation
-  // Sentence may be in other languages, so need to handle only English words
-  let words = sentence.split(/(\s+|[a-zA-Z]+)/);
+export const Sentence = memo(
+  ({ sentence, className }: { sentence: string; className?: string }) => {
+    // split by space or punctuation
+    // Sentence may be in other languages, so need to handle only English words
+    let words = sentence.split(/(\s+|[a-zA-Z]+)/);
 
-  return (
-    <span className={cn("break-words align-middle", className)}>
-      {words.map((word, index) => {
-        return (
-          <span key={index}>
-            {word.match(/[a-zA-Z]+/) ? (
-              <Vocabulary word={word} context={sentence} />
-            ) : (
-              word
-            )}
-          </span>
-        );
-      })}
-    </span>
-  );
-};
+    return (
+      <span className={cn("break-words align-middle", className)}>
+        {words.map((word, index) => {
+          return (
+            <span key={index}>
+              {word.match(/[a-zA-Z]+/) ? (
+                <Vocabulary word={word} context={sentence} />
+              ) : (
+                word
+              )}
+            </span>
+          );
+        })}
+      </span>
+    );
+  }
+);
