0614ceb83d900493b2d0ff0744724057aa7e7617
Chuck <403chuck@gmail.com>
2025-02-13 00:04:12 +0800

    fix: the document is rendered as a table due to a bug in the scroll area

--- enjoy/src/renderer/components/ui/scroll-area.tsx
+++ enjoy/src/renderer/components/ui/scroll-area.tsx
@@ -14,7 +14,7 @@ const ScrollArea = React.forwardRef<
     className={cn("relative overflow-hidden", className)}
     {...props}
   >
-    <ScrollAreaPrimitive.Viewport className="h-full w-full rounded-[inherit]">
+    <ScrollAreaPrimitive.Viewport className="h-full w-full rounded-[inherit] [&>div]:!block">
       {children}
     </ScrollAreaPrimitive.Viewport>
     <ScrollBar />
